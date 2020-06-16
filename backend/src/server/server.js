3971ab81531f9f50524aaad40328fa6c701f89aa
hudanasution <hudanasution@gmail.com>
2020-06-16 18:43:17 +0700

    backend-config cross policy -config cross policy -config useFindAndModify fot db config -add multer

--- backend/src/server/server.js
+++ backend/src/server/server.js
@@ -6,13 +6,36 @@ const app = express()
 // Bodyparser middleware
 app.use(express.json())
 
+// Add headers
+app.use((req, res, next) => {
+
+    // Website you wish to allow to connect
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+
+    // Request headers you wish to allow
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Headers', 'x-auth-token,content-type')
+    // res.setHeader('Access-Control-Allow-Headers', '*')
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true)//??
+
+    // Pass to next layer of middleware
+    next()
+})
+
 // Db config
 const db = require('./config/keys').mongoURI
 
 // connect to Monggo
 mongoose.connect(db, {
     useNewUrlParser: true, //////  it uses to parse MongoDB connection strings
-    useFindAndModify: false , ////// you can use findOneAndDelete, findOneAndUpdate & etc
+    useFindAndModify: false, ////// you can use findOneAndDelete, findOneAndUpdate & etc
     useCreateIndex: true, //////  making Mongoose use createIndex() instead. ???
     useUnifiedTopology: true, ////// introduced a significant refactor of how it handles monitoring all the servers in a replica set or sharded cluster
 })
